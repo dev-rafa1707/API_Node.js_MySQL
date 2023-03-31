b13d226b505b9c65cdecce403611611860c3b480
dev-rafa1707 <rafa1707@gmail.com>
2023-03-31 06:05:16 -0300

    Add routes router.js

--- routes/router.js
+++ routes/router.js
@@ -4,5 +4,13 @@ const controller = require("./controllers/controller")
 const router = express.Router()
 
 
+router.get("/register", controller.register)
+
+router.get("/",controller.getAll)
+router.get("/:id", controller.getById)
+router.post("/", controller.create)
+router.put("/:id", controller.update)
+router.delete("/:id", controller.delete)
+
 
 module.exports = router
